cab0a1377faa7a273fb7240cf8c1b27aaf841d56
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-10-30 17:22:17 -0700

    fix test

--- beg-module-3/tests.py
+++ beg-module-3/tests.py
@@ -46,7 +46,9 @@ class Tests(unittest.TestCase):
 
     def test_3(self, f):
         def is_prime(x):    
-            for i in range(2,int(x/2)):
+            if x<2:
+                return False
+            for i in range(2,int(x/2)+1):
                 if x % i == 0:
                     return False
             return True
