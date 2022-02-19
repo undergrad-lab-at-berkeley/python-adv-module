1a8fc57bee8528db484a003fe484a1d13f423e2f
Yi J. Zhu <yi.jeffrey.zhu@gmail.com>
2022-02-19 00:44:40 -0800

    fix tests

--- beg-module-8/tests.py
+++ beg-module-8/tests.py
@@ -18,25 +18,48 @@ class Tests(unittest.TestCase):
         self.assertTrue(np.allclose(cols, np.load('tests/1a.npy')))
 
     def test_1b(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/1b.pkl')))
-
+        N = len(df)
+        self.assertEquals(N, 118218) 
+        self.assertTrue(np.alltrue(df.columns==['RAhms','DEdms','Plx','B-V','e_B-V','Hpmag']))
+        
+        def f(x):
+            self.assertTrue(type(x)==str)
+            return x.strip()
+        strip = np.vectorize(f)
+        self.assertTrue(np.alltrue(strip(df.iloc[0:1].to_numpy()) == ['00 00 00.22', '+01 05 20.4', '3.54', '0.482', '0.025', '9.2043']))
+        self.assertTrue(np.alltrue(strip(df.iloc[N-1:N].to_numpy()) == ['23 59 54.91', '-65 34 37.5', '8.71', '-0.075', '0.011',
+        '4.4758']))
+        
+    def test_1c(self, df):
+        self.assertEquals(len(df), 118218)
+        self.assertTrue(np.alltrue((df.dtypes.to_numpy()==float) == [False, False,  True,  True,  True,  True]))
+        
     def test_1d(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/1d.pkl')))
+        self.assertEquals(len(df), 116812)
+        self.assertEquals(np.sum(np.sum(df.isnull()).to_numpy()), 0)
+        
 
     def test_1e(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/1e.pkl')))
+        self.assertEquals(len(df), 104099)
+        self.assertEquals(np.sum(df['Plx']>0), 104099)
+        self.assertEquals(np.sum(df['e_B-V']<0.05), 104099)
+
 
     def test_1f(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/1f.pkl')))
-
+        self.assertEquals(len(df), 104099)
+        self.assertTrue(np.allclose(df[df['Plx']==21.9]['Dist'].to_numpy(), 148.94977169))
+        
     def test_1g(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/1g.pkl')))
+        self.assertEquals(len(df), 104099)
+        self.assertTrue(np.allclose(df[(df['Hpmag']==8.7077) & (df['Dist']<300)]['M'].to_numpy(), 4.05419006))
 
     def test_1h(self):
         self.assertTrue('my_hr.png' in os.listdir(os.getcwd()))    
 
     def test_2a(self, df):
-        self.assertTrue(df.equals(pd.read_pickle('tests/2a.pkl')))
+        self.assertEquals(len(df), 104099)
+        self.assertEquals(df.iloc[7:8].to_numpy()[0][0], '00 00 08.48')
+        self.assertEquals(df.iloc[100:101].to_numpy()[0][0], '00 01 23.69')
 ####################
 
 
